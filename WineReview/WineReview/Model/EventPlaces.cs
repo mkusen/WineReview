6bfa082e3c3807e80ca73f936ade98001645eae3
Mario Kušen <kusen.mario@gmail.com>
2024-07-12 22:06:44 +0200

    dodan Model 12-7-24

---
+++ WineReview/WineReview/Model/EventPlaces.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WineReview.Model
+{
+    internal class EventPlaces : Entity
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string PlaceName { get; set; }
+        public string EventName { get; set; }
+    }
+}
---
+++ WineReview/WineReview/Model/Wines.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WineReview.Model
+{
+    internal class Wines : Entity
+    {
+        public string Maker { get; set; }
+        public string WineName { get; set; }
+        public string YearOfHarvest { get; set; }
+        public float Price { get; set; }
+    }
+}
